496d1aa0623c7bbf9a761d787d6b7fc954684eda
mbyhring@gmail.com <mbyhring@gmail.com@7614da9f-ff01-17f8-8524-de80b6ef34cf>
2011-11-15 23:06:28 +0000

    web: set up page skeleton with routes for basic actions

--- go/app/controllers/Application.java
+++ go/app/controllers/Application.java
@@ -2,6 +2,7 @@ package controllers;
 
 import flexjson.JSONSerializer;
 import models.Game;
+import play.data.validation.Required;
 import play.mvc.*;
 
 
@@ -11,23 +12,39 @@ public class Application extends Controller {
             render();
         }
 
-        public static void play(String player, String x, String y) {
+        public static void play(Long id, @Required String player, @Required int x, @Required int y) {
+            Game g = Game.findById(id);
+            if (validation.hasErrors()) {
+                render("Application/show.html", g);
+            }
+            System.out.println("id = " + id + " player = " + player + " ( " + x + ", " + y + ")");
+            if (g != null) {
+                char p = player.charAt(0);
+                if (g.play(p, x, y)) {
+                    flash.success("%s played %s %s", player, x, y);
+                } else {
+                    flash.error("Invalid move: %s played %s %s", player, x, y);
+                }
+            }
             System.out.println(player + " played! ( " + x + ", " + y + ")");
+            show(g.id);
 
         }
 
         public static void create(int size) {
-            Game newGame = new Game(size).save();
+            Game game = new Game(size).save();
+            show(game.id);
 
         }
 
-        public static void show() {
-            Game newGame = new Game(19).save();
-            JSONSerializer gameSerializer = new JSONSerializer().include(
-                            "isPlayer1Turn",
-                            "board"
-                            ).exclude("*");
-            renderJSON(new String(gameSerializer.serialize(newGame)));
+        public static void show(Long id) {
+            Game game = Game.findById(id);
+            render(game);
+//            JSONSerializer gameSerializer = new JSONSerializer().include(
+//                            "isPlayer1Turn",
+//                            "board.positions"
+//                            );
+//            renderJSON(new String(gameSerializer.serialize(game)));
         }
 
 
